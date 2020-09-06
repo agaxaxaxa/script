0bcc19ab46753f04d192aa3faca5864ed22eab32
agaxaxaxa <otukisan1221@gmail.com>
2020-09-06 13:59:35 +0900

    動作未確認

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
   var monster, number, idurl, statusid, status;
   monster = 150;//通常モンスター数
 
-  idurl = window.prompt("URL,または動画IDを入力してください。","")//プレイヤーにURLや動画IDを入力してもらう
+  idurl = window.prompt("URL,または動画IDを入力してください。","")//プレイヤーにURLや動画IDを入力
   const videoid = idurl.replace(/[^0-9]/g, '');//動画IDから値を抽出
 
   if (videoid == 18008585)//うんこちゃん モンスターファーム2 Part01 12/6/3
@@ -15,18 +15,17 @@
     number = videoid % monster;//余剰で参照に使用する値を計算
   }
 
-alert(number)
+  $gameParty.removeActor(0)//先頭キャラの追い出し
 
-\gameActors.actor(number). setup(number)//キャラステータスの初期化
-\gameParty. addActor(number)//該当するキャラをパーティーに追加する
+  $gameActors.actor(number). setup(number)//キャラステータスの初期化
+  $gameParty. addActor(number)//該当するキャラをパーティーに追加
 
-for (let statusid = 0; statusid < 6; statusid++)//個体ごとに異なるステータスを設定
-{
-  status = Math.floor( Math.random() * 20 );
-  \gameActors.actor(number). addParm(statusid, status);
-}
+  for (let statusid = 0; statusid < 6; statusid++)//個体ごとに異なるステータスを設定
+  {
+    status = Math.floor( Math.random() * 21 );//0～20の間でスタータスの加算する値を決定
+    $gameParty.members(0).addParm(statusid, status);//０番目にキャラへステータス情報の反映
+  }
 
-/gameMesage.add()
-
+  $gameMesage.add($gameParty.members(0).name);//パーティに追加されたキャラの名前を表示
 
 })();
